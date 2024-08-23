e7c72efe2bf4cad865dce05d4dbfd36042312cc4
Anna Hope <hello@annahope.me>
2024-08-23 12:15:08 -0500

    Encrypt a file with a given key

--- set1/src/main.zig
+++ set1/src/main.zig
@@ -1,50 +1,58 @@
 const std = @import("std");
+const fs = std.fs;
+const process = std.process;
+
 const crypto = @import("crypto.zig");
 const helpers = @import("helpers.zig");
 
+const ProgramError = error{
+    NotEnoughArguments,
+};
+
 pub fn main() !void {
+    std.debug.print("Running...\n", .{});
+
     var general_purpuse_allocator = std.heap.GeneralPurposeAllocator(.{ .safety = true }){};
     const gpa = general_purpuse_allocator.allocator();
     var arena_allocator = std.heap.ArenaAllocator.init(gpa);
     const allocator = arena_allocator.allocator();
     defer arena_allocator.deinit();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
-    try stdout.print("Running...\n", .{});
 
-    try bw.flush(); // don't forget to flush!
+    var args_iterator = try process.argsWithAllocator(allocator);
+    const program_name = args_iterator.next().?;
+    const maybe_input_path = args_iterator.next();
+    const maybe_key = args_iterator.next();
 
-    const data_path = "/Users/annahope/projects/rc/cryptopals/set1/data/4.txt";
-    const inputs = try helpers.readLines(allocator, data_path);
+    var input_path: []const u8 = undefined;
+    var key: []const u8 = undefined;
 
-    const dict_path = "/usr/share/dict/words";
-    const words = try helpers.readLines(allocator, dict_path);
+    var have_enough_args = false;
 
-    const char_freqs = try crypto.getCharFrequencies(allocator, words);
-    var final_output: ?crypto.DecryptedOutput = null;
-    var best_index: usize = 0;
-
-    for (inputs, 0..inputs.len) |input, index| {
-        const input_hex = crypto.HexString.initFromHex(input);
-        const output = try crypto.decryptXordHex(allocator, input_hex, char_freqs);
-        if (final_output) |fo| {
-            if (output.score > fo.score) {
-                final_output = output;
-                best_index = index;
-            }
-        } else {
-            final_output = output;
+    if (maybe_input_path) |some_input_path| {
+        input_path = some_input_path;
+        if (maybe_key) |some_key| {
+            key = some_key;
+            have_enough_args = true;
         }
-
-        try bw.flush();
     }
 
-    try stdout.print("best index: {d}, message: {s}, key: {c}\n", .{ best_index, final_output.?.output, final_output.?.key });
+    if (!have_enough_args) {
+        try stdout.print("Usage: {s} <relative_input_path> <encryption_key>\n", .{program_name});
+        try bw.flush();
+        return error.NotEnoughArguments;
+    }
+
+    const cwd = fs.cwd();
+    // Limit to 10 megabytes
+    const input = try cwd.readFileAlloc(allocator, input_path, 1024 * 1024 * 10);
+
+    const encrypted = try crypto.encryptRepeatingKeyXor(allocator, input, key);
+
+    try stdout.print("{s}\n", .{encrypted.buf});
 
     try bw.flush();
 }
